ef261f9526172c816b7367f13d7c8538c00ad430
Tony Borisov <tony.d.borisov@gmail.com>
2019-04-10 05:47:12 +0300

    feat: adding history npm package, code cleanup

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
 import ContentWrapper from './ContentWrapper'
 import Header from './Header'
 import Nav from './Nav'
 import PageName from './PageName'
 
+const history = createBrowserHistory({
+    basename: '/adbstudios.cc'
+})
+
 const Layout = () => {
     return (
         <div id="layout">
-            <Router>
+            <Router history={history}>
                 <Header />
                 <PageName position={'page'}/>
                 <ContentWrapper/>
