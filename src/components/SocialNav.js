1099d82d2a4fab3a7c06f5fabf938524f0ed9fb1
Tony Borisov <tony.d.borisov@gmail.com>
2019-11-04 20:26:46 +0200

    feat: social nav bar buttons are now links

--- src/components/SocialNav.js
+++ src/components/SocialNav.js
@@ -7,9 +7,15 @@ const SocialNav = (props) => {
     return (
         <section id='social-nav-container' className={positionClass}>
             <ul className='social-nav'>
-                <li>Codepen</li>
-                <li>LinkedIn</li>
-                <li>GitHub</li>
+                <li>
+                    <a href='https://codepen.io' target='_blank' rel="noopener noreferrer">Codepen</a>
+                </li>
+                <li>
+                    <a href='https://www.linkedin.com' target='_blank' rel="noopener noreferrer">LinkedIn</a>
+                </li>
+                <li>
+                    <a href='https://github.com/Todiros' target='_blank' rel="noopener noreferrer">GitHub</a>
+                </li>
             </ul>
         </section>
     )
