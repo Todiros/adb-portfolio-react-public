3cec978474f0c7c062eee8d18dbf899b2e7b0c6b
Tony Borisov <tony.d.borisov@gmail.com>
2019-11-06 20:02:14 +0200

    feat: initial homepage markup

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      <p>Home</p>
+    <div id='home-page-wrapper'>
+      <p id='welcome-msg'>Welcome!</p>
+      <section id='short-intro'>Hey, it's Anthony D. Borr</section>
+      <button id='home-page-work-btn' className='page-button'>Work</button>
     </div>
   )
 }
