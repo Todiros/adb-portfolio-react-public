32f44b2a67b0710cc4e91af70675e2581d885bad
Tony Borisov <tony.d.borisov@gmail.com>
2019-11-27 05:58:56 +0200

    feat: contact form in contact page

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 const ContactPage = () => {
   return (
-    <div>
-      <p>Contact</p>
+    <div id='contact-page-wrapper'>
+      <p>You can reach me via email using the form below.</p>
+      <form id='form-wrapper'>
+        <section id='input-wrapper'>
+          <input type="text" id="input-name" className="input-field first-input" name="name" placeholder="Name" required/>
+          <input type="email" id="input-email" className="input-field" name="email" placeholder="Email" required/>
+          <input type="text" id="input-subject" className="input-field" name="subject" placeholder="Subject" required/>
+          <textarea id="input-message" className="input-field" name="message" placeholder="Message" spellcheck="false"/>
+        </section>
+        <section id='submit-wrapper'>
+          <button id='send-mail-button' className='custom-button'>SEND</button>
+        </section>
+      </form>
     </div>
   )
 }
