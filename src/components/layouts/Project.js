707cafb50ddbf131126fdb58043f7a2ff21199ac
Tony Borisov <tony.d.borisov@gmail.com>
2019-12-18 18:07:28 +0200

    feat: prop-types, moves styling to separate file

--- src/components/layouts/Project.js
+++ src/components/layouts/Project.js
@@ -4,23 +4,31 @@ import PropTypes from 'prop-types'
 const Project = props => {
     return (
         <article className='work-project'>
-            <h4 id="project-title">Project Name</h4>
-            <img src='https://www.topsoft.am/media/1010/how-to-create-a-website-feature-image.jpg' alt="project" width="300px"/>
+            <h4 id="project-title">{props.name}</h4>
+            <img src={props.image} alt="project" className='project-thumbnail'/>
             <ul id="project-links">
-                <li className="project-live">live</li>
-                <li className="project-code">code</li>
+                <li className="project-live"><a href={props.live} target='_blank' rel='noopener noreferrer'>live</a></li>
+                <li className="project-code"><a href={props.code} target='_blank' rel='noopener noreferrer'>code</a></li>
             </ul>
-            <p className="project-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt dolorum non commodi consequatur quia libero sunt eius consequuntur illo, odit veniam. Eum ab saepe ullam ipsa dolore doloremque eaque libero.</p>
+            <p className="project-description">{props.description}</p>
         </article>
     )
 }
 
 Project.propTypes = {
-
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    description: PropTypes.string,
+    live: PropTypes.string,
+    code: PropTypes.string.isRequired
 }
 
 Project.defaultProps = {
-
+    name: "Project Default",
+    image: "https://www.topsoft.am/media/1010/how-to-create-a-website-feature-image.jpg",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt dolorum non commodi consequatur quia libero sunt eius consequuntur illo, odit veniam. Eum ab saepe ullam ipsa dolore doloremque eaque libero.",
+    live: "#",
+    code: "#"
 }
 
 export default Project
