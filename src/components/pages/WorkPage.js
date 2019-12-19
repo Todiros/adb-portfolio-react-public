23a838b65f8636cf0350df0a6062037b391104b7
Tony Borisov <tony.d.borisov@gmail.com>
2019-12-19 15:43:51 +0200

    fix: maps over a dummy arr to dynamically generate projects instead of hardcoding them

--- src/components/pages/WorkPage.js
+++ src/components/pages/WorkPage.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import Project from '../layouts/Project'
 
+const projectArr = [1, 2, 3, 4, 5, 6, 7]
 
 const WorkPage = () => {
     return (
         <div id="work-page-wrapper">
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
+            {projectArr.map(i =>
+                <Project name={'Project ' + i}/>
+            )}
         </div>
     )
 }
