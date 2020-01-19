53f8c0a6b12ef6ecf12d4ad7dadbb137a515ba68
Tony Borisov <tony.d.borisov@gmail.com>
2020-01-19 19:22:18 +0200

    feat: dummy array, skill list and basic styling

---
+++ src/components/layouts/SkillList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SkillList = props => {
+    return (
+        <ul id={props.listId}>
+            {props.children.map((item) => 
+                <li className={props.listItemClass}>{item}</li>
+            )}
+        </ul>
+    )
+}
+
+SkillList.propTypes = {
+    listId: PropTypes.string.isRequired,
+    listItemClass: PropTypes.string.isRequired,
+    listItemContent: PropTypes.array
+}
+
+export default SkillList
--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -19,6 +19,7 @@ const LoremIpsumIntro = () => <React.Fragment>
 const LoremIpsumCV = () => <React.Fragment>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus lectus nec justo suscipit auctor. Fusce mollis sapien eget quam sodales, id pharetra magna suscipit. Nulla facilisi. Integer nec nulla mi. Nam placerat nisi venenatis, consequat mauris et, ornare libero. Suspendisse potenti. Morbi turpis elit, aliquet consequat odio nec, scelerisque.
 </React.Fragment>
+const DummySkillArray = () => ["Skill000", "Skill001", "Skill002", "Skill003", "Skill004", "Skill005", "Skill006", "Skill007", "Skill008", "Skill009", "Skill010", "Skill011", "Skill012", "Skill013", "Skill014"]
 
 const AboutPage = () => {
     return (
@@ -29,24 +30,29 @@ const AboutPage = () => {
                 </p>
             </section>
             <div id='cv-wrapper'>
-                    
-                <AboutSection sectionId='cv-skills' sectionTitle='Skills'>
+                <AboutSection sectionId='cv-skills' sectionTitle='Skills'>    
                     <SkillList listId='skill-wrapper' listItemClass='skill-item'>
-                        {['Test', 'Test 2', 'Test 3']}
+                        {DummySkillArray()}
                     </SkillList>
                 </AboutSection>
-                
-                <AboutSection sectionId='cv-traits' sectionTitle='Traits'>    
+
+                <AboutSection sectionId='cv-traits' sectionTitle='Traits'>
+                    <SkillList listId='skill-wrapper' listItemClass='skill-item'>
+                        {DummySkillArray()}
+                    </SkillList>    
                 </AboutSection>
                 
                 <AboutSection sectionId='cv-talents' sectionTitle='Talents'>    
+                    <SkillList listId='skill-wrapper' listItemClass='skill-item'>
+                        {DummySkillArray()}
+                    </SkillList>
                 </AboutSection>
 
                 <AboutSection sectionId='cv-certificates' sectionTitle='Certificates'>
                 </AboutSection>
                 
                 <AboutSection sectionId='cv-education' sectionTitle='Education'>
-                </AboutSection> 
+                </AboutSection>
                 
                 <AboutSection sectionId='cv-passions' sectionTitle='Passions'>
                     <LoremIpsumCV />
@@ -55,6 +61,7 @@ const AboutPage = () => {
                 <AboutSection sectionId='cv-projects' sectionTitle='Projects'>
                     <Link to={`/work`} id='about-page-work-button' className='page-link'>go to projects</Link>
                 </AboutSection>
+
                 
                 <div id='button-wrapper'>
                     <button id='download-cv-button' className='custom-button'>Download CV</button>
