32bd9d8c0aec6f93f785628bd6df13759490987f
Tony Borisov <tony.d.borisov@gmail.com>
2020-02-07 00:45:59 +0200

    feat: new mobile layout, improvements on desktop layout

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -4,31 +4,32 @@ const ContactPage = () => {
   return (
     // Page Wrapper
     <div id='contact-page-wrapper'>
+      <div id="contact-wrapper">
 
-      {/* Contact Form */}
-      <form id='form-wrapper'>
-        <p>You can reach me via email using the form below.</p>
-        <section id='input-wrapper'>
-          <input type="text" id="input-name" className="input-field first-input" name="name" placeholder="Name" required/>
-          <input type="email" id="input-email" className="input-field" name="email" placeholder="Email" required/>
-          <input type="text" id="input-subject" className="input-field" name="subject" placeholder="Subject" required/>
-          <textarea id="input-message" className="input-field" name="message" placeholder="Message" spellCheck="false"/>
-        </section>
-        <section id='submit-wrapper'>
-          <button id='send-mail-button' className='custom-button'>SEND</button>
-        </section>
-      </form>
+        {/* Contact Form */}
+        <form id='contact-form'>
+          <p>You can reach me via email using the form below.</p>
+          <section id='input-wrapper'>
+            <input type="text" id="input-name" className="input-field first-input" name="name" placeholder="Name" required/>
+            <input type="email" id="input-email" className="input-field" name="email" placeholder="Email" required/>
+            <input type="text" id="input-subject" className="input-field" name="subject" placeholder="Subject" required/>
+            <textarea id="input-message" className="input-field" name="message" placeholder="Message" spellCheck="false"/>
+          </section>
+          <section id='submit-wrapper'>
+            <button id='send-mail-button' className='custom-button'>SEND</button>
+          </section>
+        </form>
 
-      {/* Contact Details Section */}
-      <section id='contact-details-wrapper'>
-          <h4>Contact Details</h4>
-          <ul id="contact-details">
-            <li id="contact-name">Anthony D. Borr</li>
-            <li id="contact-location">Sofia, Bulgaria</li>
-            <li id="contact-email">myemail@email.com (dummy)</li>
-          </ul>
-      </section>
-      
+        {/* Contact Details Section */}
+        <section id='contact-details-wrapper'>
+            <h4>Contact Details</h4>
+            <ul id="contact-details">
+              <li id="contact-name">Anthony D. Borr</li>
+              <li id="contact-location">Sofia, Bulgaria</li>
+              <li id="contact-email">myemail@email.com (dummy)</li>
+            </ul>
+        </section>
+      </div>
     </div>
   )
 }
