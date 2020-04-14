5f2534a74da56434cb81add428f6685a46257d75
Tony Borisov <tony.d.borisov@gmail.com>
2020-04-14 23:39:22 +0300

    feat: adds nav icons and basic styling

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -2,6 +2,12 @@ import React from 'react'
 import SocialNav from './SocialNav'
 import { Link } from 'react-router-dom'
 
+import _homeIcon from '../../res/menu/home-page-icon.svg'
+import _workIcon from '../../res/menu/work-page-icon.svg'
+import _contactIcon from '../../res/menu/contact-page-icon.svg'
+import _aboutIcon from '../../res/menu/about-page-icon.svg'
+
+
 const Nav = (props) => {
     /* 
         The component has been used twice -
@@ -17,16 +23,44 @@ const Nav = (props) => {
             <nav id='nav-container' className={positionClass}>
                 <ul id="nav">
                     <li>
-                        <Link to={`/`}>HOME</Link>
+                        <Link to={`/`}>
+                            <img 
+                                src={_homeIcon} 
+                                alt='nav-home'
+                                id='nav-icon' 
+                                className='nav-home-icon'
+                            />
+                        </Link>
                     </li>
                     <li>
-                        <Link to={`/work`}>WORK</Link> 
+                        <Link to={`/work`}>
+                            <img 
+                                src={_workIcon} 
+                                id='nav-icon' 
+                                className='nav-work-icon' 
+                                alt='nav-work'
+                            />
+                        </Link> 
                     </li>
                     <li>
-                        <Link to={`/contact`}>CONTACT</Link> 
+                        <Link to={`/contact`}>
+                            <img 
+                                src={_contactIcon} 
+                                id='nav-icon' 
+                                className='nav-contact-icon' 
+                                alt='nav-contact'
+                            />
+                        </Link> 
                     </li>
                     <li>
-                        <Link to={`/about`}>ABOUT</Link> 
+                        <Link to={`/about`}>
+                            <img 
+                                src={_aboutIcon} 
+                                id='nav-icon' 
+                                className='nav-about-icon' 
+                                alt='nav-about'
+                            />
+                        </Link> 
                     </li>
                 </ul>
                 <SocialNav position={positionClass} />
