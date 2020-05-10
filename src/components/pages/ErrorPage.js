6688dd820d3e3a89055137f6e76036a2b1cb5669
Tony Borisov <tony.d.borisov@gmail.com>
2020-05-10 07:01:09 +0300

    fix: deletes old 404 img file, renames current one

--- src/components/pages/ErrorPage.js
+++ src/components/pages/ErrorPage.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import _404 from '../../res/404_2.svg'
+import _404 from '../../res/404.svg'
 
 // Any attempt to access non-existing page will lead to this page
 const ErrorPage = () => {
     return (
         <div id="error-page-wrapper">
-            <p>Page Not Found!</p>
+            <p>Page Not Found</p>
             <img 
                 src={_404} 
                 alt='error 404 page'
