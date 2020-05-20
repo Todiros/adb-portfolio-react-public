5a470917e754ff0baea17ea00498c15426943d94
Tony Borisov <tony.d.borisov@gmail.com>
2020-05-20 04:00:01 +0300

    feat: new icon, restyling and code structure

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import _MyPhoto from '../../res/home_face.svg'
+import _MustacheGuy from '../../res/mustache_guy.svg'
 import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
     <div id='home-page-wrapper'>
       <section id='intro-wrapper'>
-        <p id='welcome-msg'>Welcome!</p>
-        <p id='short-intro'>Hey, it's Anthony D. Borr</p>
+        <h2 id='welcome-msg'>
+          Welcome! <img src={_MustacheGuy} alt='mustache guy' id='welcome-icon'/> 
+        </h2>
+        <p id='short-intro'>It's Tony here, the creator of ADB<span className="accent">Studios</span></p>
+        <p id="role">
+          and {'<'}<span className="code">Full Stack Developer</span>{'/>'}
+        </p>
         <Link to={`/work`} id='home-page-work-button' className='page-link'>Work</Link>
       </section>
       <section id='landing-wrapper'>
