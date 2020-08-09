dae3c7033ffc628365a39e9e97ec30f4bbaa8ace
Tony Borisov <tony.d.borisov@gmail.com>
2020-08-09 03:49:20 +0300

    feat: sorting of projects by Id

--- src/components/pages/WorkPage.js
+++ src/components/pages/WorkPage.js
@@ -8,12 +8,13 @@ const ProjectsData = CV.Projects
 const WorkPage = () => {
     return (
         <div id="work-page-wrapper">
-            {ProjectsData.map(project =>
-                <Project project={project} key={project.Id}/>
+            {ProjectsData
+                .sort((a, b) => a.Id > b.Id ? 1 : -1)
+                .map(project =>
+                    <Project project={project} key={project.Id}/>
             )}
         </div>
     )
 }
 
 export default WorkPage
-
