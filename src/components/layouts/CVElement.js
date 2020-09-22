f90fd6a2db4a0a0e73a2859706a6affac884fe67
Tony Borisov <tony.d.borisov@gmail.com>
2020-09-22 22:16:34 +0300

    feat: makes section titles green

--- src/components/layouts/CVElement.js
+++ src/components/layouts/CVElement.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 const CVElement = (props) => {
     return (
         <section id={props.sectionId}>
-            <h4>{props.sectionTitle}</h4>
+            <h4 id='cv-section-title'>{props.sectionTitle}</h4>
             {props.children}
         </section>
     )
