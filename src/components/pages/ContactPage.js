3be99a47d7b51677892f2421cbb6510035af8dd7
Tony Borisov <tony.d.borisov@gmail.com>
2020-09-28 00:06:48 +0300

    feat: connects contact form to Netlify

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -7,9 +7,12 @@ const ContactPage = () => {
       <div id="contact-wrapper">
 
         {/* Contact Form */}
-        <form id='contact-form'>
+        <form id='contact-form' name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
           <p>You can reach me via email using the form below.</p>
           <section id='input-wrapper'>
+            <p id="hidden-input-field">
+              <label>Don’t fill this out if you're human: <input name="bot-field"/></label>
+            </p>
             <input type="text" id="input-name" className="input-field first-input" name="name" placeholder="Name" required/>
             <input type="email" id="input-email" className="input-field" name="email" placeholder="Email" required/>
             <input type="text" id="input-subject" className="input-field" name="subject" placeholder="Subject" required/>
