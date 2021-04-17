40b7b9694e27d62fd36f2bf4a6a13158f55465de
Tony Borisov <tony.d.borisov@gmail.com>
2021-04-17 23:58:43 +0300

    feat: working browser detecting for Opera, Firefox and Edge; alert() for Edge users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react'
 import './styles/App.sass'
 
 import Layout from './components/Layout'
+
+// Opera 8 - 75 
+const isOpera = (!!window.opr && !! window.opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0
+
+// Firefox 1.0 - 82.0
+const isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window.safari.pushNotification));
+
+if (/Edg/.test(navigator.userAgent)) {
+  alert("You are using a browser (Edge) that is currently not supported by this page. Please switch to Chrome, Firefox or Opera for a better user experience. Thank you!");
+}
 
 class App extends Component {
   render() {
